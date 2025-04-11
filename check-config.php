6abeb37236c8cf441241e7d87fc16b9a6c023e57
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-04-12 03:59:48 +0900

    New: check-config.php <-- op-skeleton-2020:/asset/check.php

---
+++ check-config.php
@@ -0,0 +1,40 @@
+<?php
+/**	op-asset-boot:/check-config.php
+ *
+ * @created    2022-12-17  op-skeleton-2020:/asset/check.php
+ * @copied     2025-04-09  op-asset-boot:/check-config.php
+ * @version    1.0
+ * @package    op-asset-boot
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+//	ci.php or git.php is skip.
+$basename = basename($_SERVER['SCRIPT_NAME']);
+if( /* $basename === 'ci.php' or */ $basename === 'git.php' ){
+	return;
+}
+
+//	Get admin config.
+$config = Config::Get('admin');
+
+//	Set Admin IP-Address and Admin E-Mail Address.
+foreach( [Env::_ADMIN_IP_, Env::_ADMIN_MAIL_] as $key ){
+	//	...
+	if( empty($config[$key]) ){
+		//	...
+		if(!include('asset/boot/template/app/config-admin.phtml') ){
+			exit(__LINE__);
+		}
+	}
+}
